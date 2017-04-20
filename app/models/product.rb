86d1164edc8b6c40c56044b5d26baa856062d19e
Zachary Viggiani <zach@bigmojo.net>
2017-04-20 18:37:32 -0400

    removed extra class

--- app/models/product.rb
+++ app/models/product.rb
@@ -12,14 +12,3 @@ class Product < ApplicationRecord
 
  end
 end
-
-class Review  < ApplicationRecord
-  belongs_to :user
-  belongs_to :product
-end
-
-class User  < ActiveRecord::Base
-
-  has_many :reviews
-  has_many :products, through: :reviews
-end
